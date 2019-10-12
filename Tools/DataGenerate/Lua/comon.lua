0d187f325f41a265c153d4b77267211c2d814806
bestan <632469297@qq.com>
2019-10-12 17:58:31 +0800

    配置生成

--- Tools/DataGenerate/Lua/comon.lua
+++ Tools/DataGenerate/Lua/comon.lua
@@ -2,17 +2,14 @@ configName = "common"
 configAlias = "通用"
 tables = {
 	SkillDefine = 
-		type = "TABLE",
-		alias = "技能表",
+		type = "TABLE", alias = "技能表",
 		items = {
 			{ name = "section", type = "float", isList=true, alias = "字段1" },
 		},
-		value = "111",
 	},
 	
 	SKILL_TYPE = {
-		type = "ENUM",
-		alias = "技能类型",
+		type = "ENUM",  alias = "技能类型",
 		items = {
 			{ name = "SKILL_TYPE_BB", alias = "BB字段类型" },
 			{ name = "SKILL_TYPE_AA", alias = "AA字段类型" },
